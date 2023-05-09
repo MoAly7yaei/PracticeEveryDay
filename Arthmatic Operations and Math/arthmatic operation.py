6b23211e221fdace11cb235d82ad01e794c1e291
Mohammed Al Yahyai <45735173+MoAly7yaei@users.noreply.github.com>
2023-05-09 13:49:50 +0400

    Add files via upload
    This is My basic practice in class about arithmetic operation and mathematics in python

---
+++ Arthmatic Operations and Math/arthmatic operation.py
@@ -0,0 +1,29 @@
+
+"""
+a = 5
+b = 10
+t = 2
+
+reasult = 0
+
+def wrong(): 
+    reasult = sqrt(a**2+b**(2-2*a*b)*cos(t))
+    return reasult
+def right():
+    reasult = sqrt(a**2+b**2-(2*a*b*cos(t)))
+    return reasult
+
+print(wrong())
+print(right())
+    
+
+"""
+"""
+def rnd():
+    x = 4.3499 
+    y = 100
+    reasult = int(x * y*100)/100.0
+    return reasult
+
+print(rnd())
+"""
---
+++ Arthmatic Operations and Math/cylenderAreaVolume.py
@@ -0,0 +1,7 @@
+h = 4
+r = 6
+P = 3.14
+area = 2 * P * r * (r + h)
+volume = h * P * r**2
+print("Area is: %.2f" % area)
+print("Volume is: %.2f" % volume)
