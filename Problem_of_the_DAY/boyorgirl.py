74384e3cf5224c635ed4734a9005c83f6ee0abe7
Mohammed Al Yahyai <45735173+MoAly7yaei@users.noreply.github.com>
2023-06-18 14:23:56 +0400

    Update boyorgirl.py
    Supervised by Ahmed Al-Abri

--- Problem_of_the_DAY/boyorgirl.py
+++ Problem_of_the_DAY/boyorgirl.py
@@ -1,14 +1,13 @@
 word = input("Enter a username: ")
+lst = list(word)
 count = 0
+unique = []
+for i in lst:
+    if lst.count(i) == 1:
+        unique.append(i)
+        
+if len(unique) % 2 == 0:
+    print("She is girl")
 
-for i in word:
-    for j in word:
-        if i == j:
-            break
-        else:
-            count += 1
-
-if count % 2 == 0:
-    print("She is a girl")
 else:
-    print("He is a boy")
+    print("He is Boy")
